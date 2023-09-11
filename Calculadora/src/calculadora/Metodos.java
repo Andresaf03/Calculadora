b3d01caf5b2fa3818b2ec36b319c5bf8e00c140f
luisrafaelarguelles <luisrafaelarguelles@10.10.20.62>
2023-09-11 12:34:27 -0600

    "Revisador parentesis"

---
+++ Calculadora/src/calculadora/Metodos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+import pilagenerica.PilaA;
+
+/**
+ *
+ * @author luisrafaelarguelles
+ */
+public class Metodos {
+    private PilaA<Character> pil;
+
+    public boolean revisador(String revisa) {
+
+        int der = 0, izq = 0;
+        boolean res = false;
+        boolean ban = true;
+        int i = 0;
+
+        while (i < revisa.length() && ban) {
+            if (revisa.charAt(i) == '(') {
+                Character ch = revisa.charAt(i);
+                pil.push(ch);
+            } else if (revisa.charAt(i) == ')') {
+                if (pil.isEmpty()) {
+                    ban = false;
+                } else {
+                    pil.pop();
+                }
+            }
+            i++;
+        }
+        if (pil.isEmpty() && ban) {
+            res = true;
+        }
+        return res;
+    }
+    
+    
+}
