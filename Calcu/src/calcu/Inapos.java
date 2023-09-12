89b340225ea455255292e272724f906178dd0418
Nicolas Alvarez Ortega <nalvarezo62@gmail.com>
2023-09-11 19:57:09 -0600

    Calcula e infig

--- Calcu/src/calcu/Inapos.java
+++ Calcu/src/calcu/Inapos.java
@@ -4,6 +4,7 @@
  */
 package calcu;
 
+import static java.lang.Character.isDigit;
 import java.util.ArrayList;
 import pilas.PilaA;
 import pilas.PilaADT;
@@ -15,15 +16,18 @@ import pilas.RevisorDeParentesis;
  */
 public class Inapos{
     
-    public String InaPos(String cadena) throws Exception{
+    public ArrayList <String> InaPos(String cadena) {
         ArrayList <String> Caracteres= new ArrayList<String>(); //Creamos arraylist para almacenar numeros
         PilaADT<Character> Operadores = new PilaA(); //Creamos pila para almacenar operadores
         
         int i;
         String posfija="";
         for(i=0;i<cadena.length();i++){
-                switch(jerarquia(charAt(i))){
+                switch(jerarquia(cadena.charAt(i))){
                     case 0:
+                        posfija = posfija + cadena.charAt(i);
+                        break;
+                    case 2:
                         posfija = posfija + cadena.charAt(i);
                         break;
                     case 1:
@@ -40,14 +44,9 @@ public class Inapos{
                         }
                             
                         break;
-                    case 2:
-                        while(!Operadores.isEmpty() && Operadores.peek().jerarquia >= cadena.charAt(i).jerarquia){
-                            Caracteres.add(String.valueOf(Operadores.pop()));
-                            
-                        }
-                        Operadores.push(cadena.charAt(i));
-                        break;
                     case 3:
+                        Caracteres.add(posfija);
+                        posfija="";
                         while(!Operadores.isEmpty() && Operadores.peek().jerarquia >= cadena.charAt(i).jerarquia){
                             Caracteres.add(String.valueOf(Operadores.pop()));
                             
@@ -55,6 +54,17 @@ public class Inapos{
                         Operadores.push(cadena.charAt(i));
                         break;
                     case 4:
+                        Caracteres.add(posfija);
+                        posfija="";
+                        while(!Operadores.isEmpty() && Operadores.peek().jerarquia >= cadena.charAt(i).jerarquia){
+                            Caracteres.add(String.valueOf(Operadores.pop()));
+                            
+                        }
+                        Operadores.push(cadena.charAt(i));
+                        break;
+                    case 5:
+                        Caracteres.add(posfija);
+                        posfija="";
                         while(!Operadores.isEmpty() && Operadores.peek().jerarquia >= cadena.charAt(i).jerarquia){
                             Caracteres.add(String.valueOf(Operadores.pop()));
                             
@@ -66,8 +76,26 @@ public class Inapos{
            Caracteres.add(posfija);
            while(!Operadores.isEmpty())
                Caracteres.add(String.valueOf(Operadores.pop()));
+           return Caracteres;
       
         }
+    
+    public double calcula(ArrayList<String> Caracteres){
+        
+        int i;
+        double aux;
+        double resp=0;
+        String cadena;
+        PilaADT <Character> a = new PilaA();
+        
+       
+        for(i=0;i<Caracteres.size();i++){
+            
+        }
+
+        
+    }
+    
     
     
     }
